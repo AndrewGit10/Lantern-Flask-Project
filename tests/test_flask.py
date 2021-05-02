485f8f3995a6d028bfb242abd4a268198849bab3
Andrew MacLean <andrewmaclean@Andrews-MacBook-Pro.local>
2021-05-02 15:07:45 -0400

    updating test_flask.py

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,7 +1,34 @@
 #testing script
 import Lantern-Flask-Project
+import os
+import tempfile
 
-def test_myflask():
-	assert myfunction() == testthing, 'Problem in package'
+import pytest
+
+from flaskr import flaskr
+
+
+@pytest.fixture
+def client():
+    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+    flaskr.app.config['TESTING'] = True
+
+    with flaskr.app.test_client() as client:
+        with flaskr.app.app_context():
+            flaskr.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(flaskr.app.config['DATABASE'])
+
+
+def test_empty_db(client):
+    """Start with a blank database."""
+
+    rv = client.get('/')
+    assert b'No entries here so far' in rv.data
+
+#def test_myflask():
+#	assert myfunction() == testthing, 'Problem in package'
 
 
